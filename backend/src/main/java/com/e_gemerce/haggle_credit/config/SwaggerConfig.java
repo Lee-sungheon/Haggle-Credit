298689caeea969be489f5051f95c808dd75a95c0
elwlahd555 <elwlahd555>
2021-04-26 14:26:34 +0900

    feat : 📝 swagger.io 추가

---
+++ backend/src/main/java/com/e_gemerce/haggle_credit/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.e_gemerce.haggle_credit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket postsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("Haggle_Credit")
+				.apiInfo(apiInfo())
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Haggle_Credit")
+				.description("Haggle_Credit")
+				.termsOfServiceUrl("https://www.notion.so/oct14jh/Haggle-Credit-5504f21c3ea14758be4bc81b1c369264")
+				.license("Haggle_Credit, 개발팀-E-Gemerce")
+				.licenseUrl("khyun7621@naver.com").version("1.0").build();
+	}
+}
