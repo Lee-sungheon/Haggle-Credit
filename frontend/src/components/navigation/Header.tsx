3343f5f2bb3da55a2a4d1440af63427a50d55cc4
sondongmin0419 <dongmin.dev0419@gmail.com>
2021-04-27 16:01:58 +0900

    feat: login

--- frontend/src/components/navigation/Header.tsx
+++ frontend/src/components/navigation/Header.tsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
-import { useEffect, useRef } from "react";
-import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import Login from '../login/Login';
 
 const Container = styled.div`
   width: 100%;
@@ -9,7 +10,7 @@ const Container = styled.div`
   top: 0;
   /* background-color: ${({ theme }) => theme.color.background}; */
   z-index: 7;
-  font-family: Spoqa Han Sans,Sans-serif;
+  font-family: Spoqa Han Sans, Sans-serif;
   color: #3c4758;
   display: block;
   opacity: 1;
@@ -33,8 +34,7 @@ const LogoBox = styled.div`
   z-index: 2;
 `;
 
-const MenuList = styled.div`
-`;
+const MenuList = styled.div``;
 
 const Menu = styled.div`
   width: 80px;
@@ -48,7 +48,7 @@ const Menu = styled.div`
   cursor: pointer;
 `;
 
-const StyledLink = styled(Link).attrs(()=>({}))`
+const StyledLink = styled(Link).attrs(() => ({}))`
   color: ${({ theme }) => theme.color.text};
   text-decoration: none;
   font-size: 14px;
@@ -58,47 +58,64 @@ const StyledLink = styled(Link).attrs(()=>({}))`
 `;
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const navRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     function myFunction() {
       if (null !== navRef.current) {
         if (window.pageYOffset > 0) {
-          navRef.current.style.backgroundColor = "white";
-          navRef.current.style.boxShadow = "0 4px 8px 0 rgb(0 0 0 / 4%)";
+          navRef.current.style.backgroundColor = 'white';
+          navRef.current.style.boxShadow = '0 4px 8px 0 rgb(0 0 0 / 4%)';
         } else {
-          navRef.current.style.backgroundColor = "inherit";
-          navRef.current.style.boxShadow = "none";
-        };
-      };
-    };
-    window.addEventListener('scroll', function() {
+          navRef.current.style.backgroundColor = 'inherit';
+          navRef.current.style.boxShadow = 'none';
+        }
+      }
+    }
+    window.addEventListener('scroll', function () {
       myFunction();
     });
-    return window.removeEventListener('scroll', function() {
+    return window.removeEventListener('scroll', function () {
       myFunction();
     });
-  }, [navRef])
+  }, [navRef]);
 
   return (
     <Container ref={navRef}>
       <HeaderContainer>
-        <LogoBox >
-          <Link to={"/home"}>
+        <LogoBox>
+          <Link to={'/home'}>
             <img
-              src={"../images/logo.png"}
-              style={{ height: "58px", marginRight: "10px" }}
+              src={'../images/logo.png'}
+              style={{ height: '58px', marginRight: '10px' }}
               alt="logo"
             />
           </Link>
         </LogoBox>
         <MenuList>
-          <Menu><StyledLink to={"/home"}>회원가입</StyledLink></Menu>
-          <Menu><StyledLink to={"/home"}>로그인</StyledLink></Menu>
-          <Menu><StyledLink to={"/home"}>내상점</StyledLink></Menu>
+          <Menu>
+            <StyledLink to={'/home'}>회원가입</StyledLink>
+          </Menu>
+          <Menu onClick={handleOpen}>
+            <p>로그인</p>
+          </Menu>
+          <Menu>
+            <StyledLink to={'/home'}>내상점</StyledLink>
+          </Menu>
         </MenuList>
       </HeaderContainer>
+      <Login open={open} handleClose={handleClose} />
     </Container>
-  )
-}
+  );
+};
 
 export default Header;
