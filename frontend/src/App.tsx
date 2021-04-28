5e45ed1b9453f818e21ca2b35d32fd0faa60f3fe
Lee-sungheon <leesungheon.dev@gmail.com>
2021-04-28 18:01:45 +0900

    feat:📝navigation bar develop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,31 @@
-import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { persistStore } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider } from 'styled-components';
-import store from './common/store';
 import Header from './components/navigation/Header';
 import NavBar from './components/navigation/NavBar';
 import IndexPage from './page/home/IndexPage';
 import Signup from './page/signup/Signup';
 import { theme } from './styles/theme';
+import { useSelector } from 'react-redux';
+import { RootState } from './common/store';
 
-const persistor = persistStore(store);
 
 const App = () => {
+  const isIndex = useSelector((state: RootState) => state.common.isIndex);
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <Header />
-            <Switch>
-              <Route exact path="/" component={IndexPage} />
-            </Switch>
-            <Switch>
-              <Route path="/home">
-                <NavBar />
-              </Route>
-            </Switch>
-            <Switch>
-              <Route path="/signup" component={Signup} />
-            </Switch>
-          </PersistGate>
-        </Provider>
+        <Header />
+        {!isIndex && <NavBar />}
+        <Switch>
+          <Route exact path="/" component={IndexPage} />
+        </Switch>
+        <Switch>
+          <Route path="/home">
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path="/signup" component={Signup} />
+        </Switch>
       </ThemeProvider>
     </BrowserRouter>
   );
