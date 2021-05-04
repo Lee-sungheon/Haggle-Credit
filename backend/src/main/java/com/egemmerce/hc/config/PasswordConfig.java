475a37df14cc865e1de74336ad8650240d1225db
elwlahd555 <elwlahd555>
2021-05-04 14:10:56 +0900

    fix : 🐞 이메일 전송관련 버그 수정

---
+++ backend/src/main/java/com/egemmerce/hc/config/PasswordConfig.java
@@ -0,0 +1,14 @@
+package com.egemmerce.hc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class PasswordConfig {
+
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//	}
+
+}
