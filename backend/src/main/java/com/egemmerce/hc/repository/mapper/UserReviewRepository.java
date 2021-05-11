97ca08ff20e46f074edb1caf9c371e163c5d51a9
goldeneastlight <goldeneastlight@gmail.com>
2021-05-11 23:21:41 +0900

    feat: ✍ UserReviewRepository add for user review

---
+++ backend/src/main/java/com/egemmerce/hc/repository/mapper/UserReviewRepository.java
@@ -0,0 +1,11 @@
+package com.egemmerce.hc.repository.mapper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.egemmerce.hc.repository.dto.UserReview;
+
+@Transactional
+public interface UserReviewRepository extends JpaRepository<UserReview, Long>{
+
+}
