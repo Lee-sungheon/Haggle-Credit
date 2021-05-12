263a472b1bd8100be18a1a12e31c569c944cd9a9
goldeneastlight <goldeneastlight@gmail.com>
2021-05-12 21:47:36 +0900

    feat: ❤ BookmarkRepository add for bookmark(찜~)

--- backend/src/main/java/com/egemmerce/hc/repository/dto/Bookmark.java
+++ backend/src/main/java/com/egemmerce/hc/repository/dto/Bookmark.java
@@ -11,27 +11,33 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@Entity @EqualsAndHashCode(of = "bItemNo")
-@Builder @AllArgsConstructor @NoArgsConstructor
+@Entity
+@EqualsAndHashCode(of = "bItemNo")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @DynamicInsert
 @DynamicUpdate
 public class Bookmark {
-	
-    @Id
+
+	@Id
 	private int bItemNo;
 	private int bUserNo;
+
 	public int getbItemNo() {
 		return bItemNo;
 	}
+
 	public void setbItemNo(int bItemNo) {
 		this.bItemNo = bItemNo;
 	}
+
 	public int getbUserNo() {
 		return bUserNo;
 	}
+
 	public void setbUserNo(int bUserNo) {
 		this.bUserNo = bUserNo;
 	}
-	
 
 }
