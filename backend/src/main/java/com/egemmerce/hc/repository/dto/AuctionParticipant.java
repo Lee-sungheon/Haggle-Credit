2c7a3163f2c81df396a98790871ef4b68d02a59a
elwlahd555 <elwlahd555>
2021-05-12 14:49:58 +0900

    fix : 🐞 회원가입 에러 수정

--- backend/src/main/java/com/egemmerce/hc/repository/dto/AuctionParticipant.java
+++ backend/src/main/java/com/egemmerce/hc/repository/dto/AuctionParticipant.java
@@ -5,6 +5,9 @@ import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -12,6 +15,8 @@ import lombok.NoArgsConstructor;
 
 @Entity @EqualsAndHashCode(of = "apNo")
 @Builder @AllArgsConstructor @NoArgsConstructor
+@DynamicInsert
+@DynamicUpdate
 public class AuctionParticipant {
 	
     @Id
