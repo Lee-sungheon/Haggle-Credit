c8b1c2718da52298434facf97fc49435661eb4b5
goldeneastlight <goldeneastlight@gmail.com>
2021-05-13 01:18:41 +0900

    feat: 🖼 user profile image select for chatting

--- backend/src/main/java/com/egemmerce/hc/repository/mapper/ItemPhotoRepository.java
+++ backend/src/main/java/com/egemmerce/hc/repository/mapper/ItemPhotoRepository.java
@@ -1,5 +1,7 @@
 package com.egemmerce.hc.repository.mapper;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,5 +9,7 @@ import com.egemmerce.hc.repository.dto.ItemPhoto;
 
 @Transactional
 public interface ItemPhotoRepository extends JpaRepository<ItemPhoto, Long>{
+
+	List<ItemPhoto> findAllByipItemNo(int ipItemNo);
 
 }
