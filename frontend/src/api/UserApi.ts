9965d75d571c0b1b452b77d45b10eafb16d5dffa
sondongmin0419 <dongmin.dev0419@gmail.com>
2021-05-12 15:06:35 +0900

    fix : signup API연결

--- frontend/src/api/UserApi.ts
+++ frontend/src/api/UserApi.ts
@@ -1,17 +1,5 @@
 import axios from 'axios';
 
-export function callApiMovieList() {
-  const url: string = `netcha/movie/list`;
-  return axios
-    .get(url)
-    .then((Response) => {
-      return Response.data;
-    })
-    .catch((Error) => {
-      console.log(Error);
-    });
-}
-
 export function userLogin(body: any) {
   const url: string = 'https://k4d107.p.ssafy.io/haggle-credit/user/login';
   return axios.post(url, body, {
@@ -19,4 +7,18 @@ export function userLogin(body: any) {
       'Content-Type': 'application/json',
     },
   });
+}
+
+export function userSignup(body: any) {
+  const url: string = 'https://k4d107.p.ssafy.io/haggle-credit/user/join';
+  return axios.post(url, body, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
+export function checkUserEmail(userEmail: any) {
+  const url: string = `https://k4d107.p.ssafy.io/haggle-credit/user/check/id?uEmail=${userEmail}`;
+  return axios.get(url);
 }
