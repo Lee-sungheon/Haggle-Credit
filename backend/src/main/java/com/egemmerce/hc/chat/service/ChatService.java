cc77f71c1a847a59e640dd7316bd158d5b6f4f64
goldeneastlight <goldeneastlight@gmail.com>
2021-05-13 22:58:31 +0900

    feat: 💋 ChatService add for chatting

--- backend/src/main/java/com/egemmerce/hc/chat/service/ChatService.java
+++ backend/src/main/java/com/egemmerce/hc/chat/service/ChatService.java
@@ -2,15 +2,22 @@ package com.egemmerce.hc.chat.service;
 
 import java.util.List;
 
+import com.egemmerce.hc.repository.dto.ChatRoom;
 import com.egemmerce.hc.repository.dto.ItemChatting;
 
 public interface ChatService {
 
-	List<ItemChatting> selectChatByicName(String string);
+	List<ItemChatting> selectChatByicCrNo(int crNo);
 
 	ItemChatting createItemChat(ItemChatting itemChatting);
 
 	List<ItemChatting> selectMainChatByicItemNoAndicMainUserNo(int icItemNo, int icMainUserNo);
 
-
+	ChatRoom createChatRoom(ChatRoom ChatRoom);
+	
+	ChatRoom selectBycrName(String crName);
+	
+	List<ChatRoom> selectChatRoomByuNo(int uNo);
+	
+	ChatRoom selectBycrNo(int crNo);
 }
