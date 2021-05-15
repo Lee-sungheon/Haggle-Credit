e9fa84970c13b62f4f5dc77bf80436bef4423ffc
goldeneastlight <goldeneastlight@gmail.com>
2021-05-16 01:40:10 +0900

    feat: ✍ user info select add

--- backend/src/main/java/com/egemmerce/hc/repository/mapper/UserReviewRepository.java
+++ backend/src/main/java/com/egemmerce/hc/repository/mapper/UserReviewRepository.java
@@ -1,16 +1,17 @@
 package com.egemmerce.hc.repository.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.egemmerce.hc.repository.dto.UserReview;
 
 @Transactional
-public interface UserReviewRepository extends JpaRepository<UserReview, Long>{
-
-	List<UserReview> findAllByurUserNo(int urUserNo);
+public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
 
 	UserReview findByurNo(int urNo);
 
