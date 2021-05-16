d3bd92293354366acf781d0b2f8b0da5be7d22bf
goldeneastlight <goldeneastlight@gmail.com>
2021-05-17 00:38:05 +0900

    feat: ✍ select different bookmarks by type

--- backend/src/main/java/com/egemmerce/hc/bookmark/service/BookmarkService.java
+++ backend/src/main/java/com/egemmerce/hc/bookmark/service/BookmarkService.java
@@ -1,6 +1,7 @@
 package com.egemmerce.hc.bookmark.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.egemmerce.hc.repository.dto.Bookmark;
 
@@ -19,13 +20,16 @@ import com.egemmerce.hc.repository.dto.Bookmark;
 public interface BookmarkService {
 	/* C :: 북마크 생성 */
 	Bookmark insertBookmark(Bookmark bookmark) throws Exception;
+
+	/* R :: 삽니다 북마크 조회 */
+	List<Map<String, Object>> selectBuyBookmark(int uNo) throws Exception;
 	
-	/* R :: 북마크 조회 */
-	List<Bookmark> selectBookmark(int uNo) throws Exception;
-	
+	/* R :: 팝니다 북마크 조회 */
+	List<Map<String, Object>> selectSellBookmark(int uNo) throws Exception;
+
 	/* D :: 북마크 삭제 */
 	int deleteBookmark(int bItemNo, int bUserNo) throws Exception;
-	
+
 	/* R :: 좋아요 상태 조회 */
 	public Bookmark selectBookmarkStatus(Bookmark bookmark) throws Exception;
 
--- backend/src/main/java/com/egemmerce/hc/repository/mapper/BookmarkMapper.java
+++ backend/src/main/java/com/egemmerce/hc/repository/mapper/BookmarkMapper.java
@@ -1,5 +1,8 @@
 package com.egemmerce.hc.repository.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.egemmerce.hc.repository.dto.Bookmark;
@@ -7,6 +10,13 @@ import com.egemmerce.hc.repository.dto.Bookmark;
 @Mapper
 public interface BookmarkMapper {
 	public Bookmark selectBookmarkStatus(Bookmark bookmark) throws Exception;
-	
+
 	public int deleteBybItemNoAndBUserNo(int bItemNo, int bUserNo) throws Exception;
+
+	/* R :: 삽니다 북마크 조회 */
+	public List<Map<String, Object>> selectBookmarkAndItemBuy(int uNo);
+	
+	/* R :: 팝니다 북마크 조회 */
+	public List<Map<String, Object>> selectBookmarkAndItemSell(int uNo);
+	
 }
