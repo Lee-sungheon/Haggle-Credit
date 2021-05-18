cdbefd42bdffc16750fd55d2941c3f81e8523a29
elwlahd555 <elwlahd555>
2021-05-19 01:05:16 +0900

    feat : 📝 @ManyToOne 추가

--- backend/src/main/java/com/egemmerce/hc/repository/dto/AuctionParticipant.java
+++ backend/src/main/java/com/egemmerce/hc/repository/dto/AuctionParticipant.java
@@ -1,38 +1,50 @@
 package com.egemmerce.hc.repository.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@Entity @EqualsAndHashCode(of = "apNo")
-@Builder @AllArgsConstructor @NoArgsConstructor
+@Entity
+@EqualsAndHashCode(of = "apNo")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @DynamicInsert
 @DynamicUpdate
 public class AuctionParticipant {
-	
-    @Id
+
 	private int apNo;
-    @Column(name="ap_item_no")
+	@Id
+	@Column(name = "ap_item_no")
 	private int apItemNo;
-    @Column(name = "ap_user_no")
+	@Column(name = "ap_user_no")
 	private int apUserNo;
 	private int apBid;
 	private LocalDateTime apDate;
 	private int apAddress;
-	
+	@ManyToOne
+	@JoinColumn(name = "ap_item_no", insertable = false, updatable = false)
+	private ItemSell itemSell;
+
 	public void generateapDate() {
-		this.apDate=LocalDateTime.now();
+		this.apDate = LocalDateTime.now();
 	}
 
 	public int getApNo() {
@@ -83,7 +95,12 @@ public class AuctionParticipant {
 		this.apAddress = apAddress;
 	}
 
+	public ItemSell getItemSell() {
+		return itemSell;
+	}
 
-	
-	
+	public void setItemSell(ItemSell itemSell) {
+		this.itemSell = itemSell;
+	}
+
 }
