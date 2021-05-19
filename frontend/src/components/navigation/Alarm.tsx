e00889ec1c343968322e4c6e56e3b1943cc93c53
Lee-sungheon <leesungheon.dev@gmail.com>
2021-05-19 22:08:20 +0900

    fix: fix image problem

--- frontend/src/components/navigation/Alarm.tsx
+++ frontend/src/components/navigation/Alarm.tsx
@@ -31,17 +31,6 @@ const DestinationCloseButton = styled.div`
   background: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDE2IDE2Ij4KICAgIDxwYXRoIGZpbGw9IiMxRTFEMjkiIGZpbGwtcnVsZT0iZXZlbm9kZCIgZD0iTTkuMTMxIDhsNS4wMzQgNS4wMzRjLjE1LjE1LjIzNS4zNTMuMjM1LjU2NSAwIC4yMTItLjA4NC40MTYtLjIzNS41NjYtLjE1LjE1LS4zNTMuMjM1LS41NjUuMjM1LS4yMTIgMC0uNDE2LS4wODUtLjU2Ni0uMjM1TDggOS4xM2wtNS4wMzUgNS4wMzRjLS4zMTIuMzEyLS44MTguMzEyLTEuMTMgMC0uMzEzLS4zMTMtLjMxMy0uODE5IDAtMS4xMzFMNi44NjggOCAxLjgzNCAyLjk2NWMtLjIwMi0uMjAyLS4yOC0uNDk2LS4yMDctLjc3Mi4wNzQtLjI3Ni4yOS0uNDkyLjU2Ni0uNTY2LjI3Ni0uMDc0LjU3LjAwNS43NzIuMjA3TDggNi44NjhsNS4wMzQtNS4wMzRjLjMxMy0uMzEyLjgyLS4zMTIgMS4xMzEgMCAuMzEzLjMxMy4zMTMuODIgMCAxLjEzMUw5LjEzMSA4eiIvPgo8L3N2Zz4K) center center no-repeat;
 `;
 
-const DestinationAddButton = styled.div`
-  cursor: pointer;
-  position: absolute;
-  display: flex;
-  top: 0.5rem;
-  right: 3.4rem;
-  width: 2.5rem;
-  height: 2.5rem;
-  background: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDE2IDE2Ij4KICAgIDxwYXRoIGZpbGw9IiMxRTFEMjkiIGZpbGwtcnVsZT0iZXZlbm9kZCIgZD0iTTE0LjExMiAxLjg4OEMxMi45NzUuNzUgMTEuMTMuNzUgOS45OTIgMS44ODhsLTguNDg5IDguNDljLS4zLjMtLjQ2OS43MDYtLjQ2OSAxLjEzdjEuODU4YzAgLjg4My43MTcgMS42IDEuNiAxLjZoMS44NjdjLjQyNSAwIC44MzItLjE3IDEuMTMyLS40N2w4LjQ4LTguNDg5LjExNy0uMTI0YzEuMDE5LTEuMTQ0Ljk4LTIuODk5LS4xMTgtMy45OTV6TTguOTA1IDUuMjM3bDEuODU3IDEuODU3LTYuMjcxIDYuMjcySDMuMDM0Yy0uMjIgMC0uNC0uMTgtLjQtLjR2LTEuNDU3bDYuMjctNi4yNzJ6bTIuMjItMi4yMThjLjUxMi0uNTEzIDEuMzQzLS41MTMgMS44NTYgMCAuNTEzLjUxMy41MTMgMS4zNDQgMCAxLjg1N2wtMS4wODcgMS4wODYtMS44NTctMS44NTcgMS4wODctMS4wODZ6Ii8+Cjwvc3ZnPgo=) center center no-repeat;
-`;
-
 const DestinationBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -105,7 +94,6 @@ const Alarm = () => {
           <div className={classes.paper}>
             <DestinationTitle>
               알림
-              <DestinationAddButton />
               <DestinationCloseButton onClick={handleClose}/>
             </DestinationTitle>
             <DestinationBox>
